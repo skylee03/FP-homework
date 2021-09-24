52cf87cf9bdb1513456646f17da0b75cc563065d
skylee03 <1178715749@qq.com>
2021-09-24 15:21:10 +0800

    update 0922 Q4

--- 0922/HW3_2.hs
+++ 0922/HW3_2.hs
@@ -1,5 +1,26 @@
 -- 猜数字小游戏
+-- 遇到问题：ghc-pkg: Couldn't open database
+-- 解决方式：将 WSL 1 升级为 WSL 2
 module Main where
+  import System.Random
 
-main :: IO ()
-main = _
+  main :: IO ()
+  main = do
+    x <- randomRIO (1, 100)
+    let
+      -- guess :: IO ()
+      guess = do
+        putStr "Please input an integer: "
+        input <- getLine
+        let x' = (read input :: Int)
+        if x' == x then
+          do putStrLn "You win!"
+        else
+          if x' < x then
+            do putStrLn "Too small!"
+               guess
+          else
+            do putStrLn "Too large!"
+               guess
+          
+    guess
